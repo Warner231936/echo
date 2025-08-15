e5630b25c724a846e634c5b722a07d686c7d8cbf
WARNER <101828275+Warner231936@users.noreply.github.com>
2025-08-15 10:52:46 -0700

    Delete paradoxcontrol directory

--- paradoxcontrol/__init__.py
+++
@@ -1,8 +0,0 @@
-from .lattice import TV, tv_join, tv_meet, tv_neg, tv_from_sc
-from .anchors import Anchor, AnchorSet
-from .engine import Evidence, Node, Graph, Evaluator, FixpointConfig
-from .policy import Guardrails
-from .bridge import (
-    load_anchors, select_and_stabilize, weave_anchor_block,
-    quick_truth, ActiveSelection
-)
